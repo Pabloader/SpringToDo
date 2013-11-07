f0e4ed497c3111bfb90bdeb2c386bec74de24adf
Pabloader <pasha.harchenko@gmail.com>
2013-11-07 21:02:17 +0400

    Нулевой коммит

---
+++ src/main/java/ru/ifmo/springmvc/dao/ContactDAOImpl.java
@@ -0,0 +1,36 @@
+package ru.ifmo.springmvc.dao;
+
+import java.util.List;
+import org.hibernate.SessionFactory;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Repository;
+import ru.ifmo.springmvc.domain.Contact;
+
+@Repository
+public class ContactDAOImpl implements ContactDAO {
+
+    @Autowired
+    private SessionFactory sessionFactory;
+
+    @Override
+    public void addContact(Contact contact) {
+        sessionFactory.getCurrentSession().save(contact);
+    }
+
+    @Override
+    public List<Contact> listContact() {
+
+        return sessionFactory.getCurrentSession().createQuery("from Contact")
+                .list();
+    }
+
+    @Override
+    public void removeContact(Integer id) {
+        Contact contact = (Contact) sessionFactory.getCurrentSession().load(
+                Contact.class, id);
+        if (null != contact) {
+            sessionFactory.getCurrentSession().delete(contact);
+        }
+
+    }
+}
